d83ac8d73f5ae600e1dd43bf88de78cea2042a4c
Gaston69 <scogaston@gmail.com>
2020-01-20 14:35:26 +0100

    red color

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PokerFormComponent } from './poker-form/poker-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PokerFormComponent
+  ],
+  imports: [
+    NgbModule,
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
